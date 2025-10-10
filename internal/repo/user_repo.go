947161194dd99d3847cfafc08fc9ba1fd6574e54
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 20:57:42 +0530

    Update user_repo.go

--- internal/repo/user_repo.go
+++ internal/repo/user_repo.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"errors"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/Vatsal-Panjiar/delivery_management_system/internal/models"
 )
@@ -13,22 +15,18 @@ func NewUserRepo(db *sqlx.DB) *UserRepo {
 	return &UserRepo{DB: db}
 }
 
-// Create inserts a new user
 func (r *UserRepo) Create(u *models.User) error {
-	_, err := r.DB.Exec(
-		`INSERT INTO users (id, username, password_hash, role, created_at)
-		 VALUES ($1,$2,$3,$4,$5)`,
-		u.ID, u.Username, u.PasswordHash, u.Role, u.CreatedAt,
-	)
+	query := `INSERT INTO users (id, username, password_hash, role, created_at)
+			  VALUES ($1,$2,$3,$4,$5)`
+	_, err := r.DB.Exec(query, u.ID, u.Username, u.PasswordHash, u.Role, u.CreatedAt)
 	return err
 }
 
-// GetByUsername fetches a user by username
 func (r *UserRepo) GetByUsername(username string) (*models.User, error) {
 	var u models.User
 	err := r.DB.Get(&u, "SELECT * FROM users WHERE username=$1", username)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("user not found")
 	}
 	return &u, nil
 }
