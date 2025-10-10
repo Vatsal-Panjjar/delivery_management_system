d8dea4f591e086c9e1a3b444d73a519694c843bd
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 15:19:57 +0530

    Create redis.go

---
+++ internal/cache/redis.go
@@ -0,0 +1,27 @@
+package cache
+
+
+import (
+"context"
+"github.com/go-redis/redis/v9"
+"time"
+)
+
+
+type RedisCache struct{
+Client *redis.Client
+Ctx context.Context
+}
+
+
+func NewRedis(addr string) *RedisCache {
+r := redis.NewClient(&redis.Options{Addr: addr})
+return &RedisCache{Client: r, Ctx: context.Background()}
+}
+
+
+func (r *RedisCache) Set(key string, value interface{}, ttl time.Duration) error {
+return r.Client.Set(r.Ctx, key, value, ttl).Err()
+}
+func (r *RedisCache) Get(key string) (string, error) { return r.Client.Get(r.Ctx, key).Result() }
+func (r *RedisCache) Del(key string) error { return r.Client.Del(r.Ctx, key).Err() }
