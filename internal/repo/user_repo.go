bc36f12386810421654dca175d630dec58ee1bbb
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 21:42:21 +0530

    Update user_repo.go

--- internal/repo/user_repo.go
+++ internal/repo/user_repo.go
@@ -1,6 +1,10 @@
 package repo
 
 import (
+	"database/sql"
+	"errors"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/Vatsal-Panjiar/delivery_management_system/internal/models"
 )
@@ -13,16 +17,20 @@ func NewUserRepo(db *sqlx.DB) *UserRepo {
 	return &UserRepo{DB: db}
 }
 
-func (r *UserRepo) GetByUsername(username string) (*models.User, error) {
-	var u models.User
-	err := r.DB.Get(&u, "SELECT * FROM users WHERE username=$1", username)
-	if err != nil {
-		return nil, err
-	}
-	return &u, nil
+func (r *UserRepo) Create(user *models.User) error {
+	_, err := r.DB.Exec(
+		`INSERT INTO users (id, username, password_hash, role, created_at)
+		 VALUES ($1,$2,$3,$4,$5)`,
+		user.ID, user.Username, user.PasswordHash, user.Role, time.Now(),
+	)
+	return err
 }
 
-func (r *UserRepo) Create(u *models.User) error {
-	_, err := r.DB.Exec("INSERT INTO users (id, username, password_hash, role, created_at) VALUES ($1,$2,$3,$4,$5)", u.ID, u.Username, u.PasswordHash, u.Role, u.CreatedAt)
-	return err
+func (r *UserRepo) GetByUsername(username string) (*models.User, error) {
+	var user models.User
+	err := r.DB.Get(&user, "SELECT * FROM users WHERE username=$1", username)
+	if err == sql.ErrNoRows {
+		return nil, errors.New("user not found")
+	}
+	return &user, err
 }
