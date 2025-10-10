bac96643b5d0001210565a68ad42294b77b9d579
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 21:09:44 +0530

    Update delivery_repo.go

--- internal/repo/delivery_repo.go
+++ internal/repo/delivery_repo.go
@@ -18,11 +18,12 @@ func NewDeliveryRepo(db *sqlx.DB) *DeliveryRepo {
 }
 
 func (r *DeliveryRepo) Create(d *models.Delivery) error {
-	query := `INSERT INTO deliveries (id, customer_id, courier_id, pickup_address, dropoff_address, status, price_cents, created_at, updated_at)
-	VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`
-	_, err := r.DB.Exec(query, d.ID, d.CustomerID, d.CourierID, d.PickupAddress, d.DropoffAddress, d.Status, d.PriceCents, d.CreatedAt, d.UpdatedAt)
-	return err
+    query := `INSERT INTO deliveries (id, customer_id, courier_id, pickup_address, dropoff_address, status, price_cents, created_at, updated_at)
+              VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`
+    _, err := r.DB.Exec(query, d.ID, d.CustomerID, d.CourierID, d.PickupAddress, d.DropoffAddress, d.Status, d.PriceCents, time.Now(), time.Now())
+    return err
 }
+
 
 func (r *DeliveryRepo) GetByID(id string) (*models.Delivery, error) {
 	var d models.Delivery
