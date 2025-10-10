3caf77deb3ed21ef2e2faf9b36b9ba8fc8e509b2
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 18:44:51 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,8 @@ import (
 )
 
 func main() {
-	db, err := sqlx.Connect("postgres", "postgres://user:pass@localhost:5432/delivery?sslmode=disable")
+	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=rupupuru@01 dbname=delivery sslmode=disable")
+
 	if err != nil {
 		panic(err)
 	}
