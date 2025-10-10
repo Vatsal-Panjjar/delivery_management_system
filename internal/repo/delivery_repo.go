ef29cc21630e0fd3e6e4315f1aac2a314889b310
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 21:12:36 +0530

    Update delivery_repo.go

--- internal/repo/delivery_repo.go
+++ internal/repo/delivery_repo.go
@@ -18,12 +18,11 @@ func NewDeliveryRepo(db *sqlx.DB) *DeliveryRepo {
 }
 
 func (r *DeliveryRepo) Create(d *models.Delivery) error {
-    query := `INSERT INTO deliveries (id, customer_id, courier_id, pickup_address, dropoff_address, status, price_cents, created_at, updated_at)
+	query := `INSERT INTO deliveries (id, customer_id, courier_id, pickup_address, dropoff_address, status, price_cents, created_at, updated_at)
               VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`
-    _, err := r.DB.Exec(query, d.ID, d.CustomerID, d.CourierID, d.PickupAddress, d.DropoffAddress, d.Status, d.PriceCents, time.Now(), time.Now())
-    return err
+	_, err := r.DB.Exec(query, d.ID, d.CustomerID, d.CourierID, d.PickupAddress, d.DropoffAddress, d.Status, d.PriceCents, time.Now(), time.Now())
+	return err
 }
-
 
 func (r *DeliveryRepo) GetByID(id string) (*models.Delivery, error) {
 	var d models.Delivery
@@ -34,7 +33,7 @@ func (r *DeliveryRepo) GetByID(id string) (*models.Delivery, error) {
 	return &d, err
 }
 
-func (r *DeliveryRepo) UpdateStatus(id, status string, courierID *int) error {
+func (r *DeliveryRepo) UpdateStatus(id, status string, courierID *string) error {
 	_, err := r.DB.Exec("UPDATE deliveries SET status=$1, courier_id=$2, updated_at=$3 WHERE id=$4", status, courierID, time.Now(), id)
 	return err
 }
