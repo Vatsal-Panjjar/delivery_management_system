e3df3d55954883d2f8cac4dfd8464d1812d37ed9
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 16:00:45 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,12 @@
-module delivery_management_system
+module github.com/Vatsal-Panjiar/delivery_management_system
 
-
-go 1.21
-
+go 1.22
 
 require (
-github.com/go-chi/chi/v5 v5.0.8
-github.com/go-redis/redis/v9 v9.0.0
-github.com/jmoiron/sqlx v1.4.10
-github.com/lib/pq v1.10.7
-github.com/golang-jwt/jwt/v5 v5.0.0
-github.com/google/uuid v1.4.0
+    github.com/go-chi/chi/v5 v5.0.8
+    github.com/redis/go-redis/v9 v9.14.0
+    github.com/jmoiron/sqlx v1.4.10
+    github.com/lib/pq v1.10.7
+    github.com/golang-jwt/jwt/v5 v5.0.0
+    github.com/google/uuid v1.4.0
 )
