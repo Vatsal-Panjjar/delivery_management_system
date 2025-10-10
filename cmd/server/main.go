4b379d233dd41f5995ae884b773d13ea74384a40
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 21:14:19 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jmoiron/sqlx"
@@ -16,34 +15,30 @@ import (
 )
 
 func main() {
+	// --- PostgreSQL Connection ---
 	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=YOUR_PASSWORD dbname=delivery sslmode=disable")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Postgres connection error: %v", err)
 	}
 
-	// Repositories
-	userRepo := repo.NewUserRepo(db)
-	deliveryRepo := repo.NewDeliveryRepo(db)
-
-	// Redis cache
+	// --- Redis Connection ---
 	rCache := cache.NewRedisCache("localhost:6379")
 
-	// Handlers
-	authHandler := handlers.NewAuthHandler(userRepo)
+	// --- Repos ---
+	deliveryRepo := repo.NewDeliveryRepo(db)
+	userRepo := repo.NewUserRepo(db)
+
+	// --- Handlers ---
 	deliveryHandler := handlers.NewDeliveryHandler(deliveryRepo, rCache)
+	authHandler := handlers.NewAuthHandler(userRepo, []byte("mysecretkey123")) // JWT secret
 
-	// Router
+	// --- Router ---
 	router := chi.NewRouter()
-	handlers.RegisterAuthRoutes(router, authHandler)
-	handlers.RegisterDeliveryRoutes(router, deliveryHandler)
 
-	fmt.Println("Server running on :8080")
-	srv := &http.Server{
-		Addr:         ":8080",
-		Handler:      router,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
+	// Auth Routes
+	router.Post("/register", authHandler.Register)
+	router.Post("/login", authHandler.Login)
 
-	log.Fatal(srv.ListenAndServe())
-}
+	// Delivery Routes
+	router.Post("/deliveries", deliveryHandler.Create)
+	router.Get("/deliveries", deliver
