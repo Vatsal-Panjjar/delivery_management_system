f826e4a60db7d9ecc7c18f08614671f2945ab753
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 20:52:34 +0530

    Create user_repo.go

---
+++ internal/repo/user_repo.go
@@ -0,0 +1,34 @@
+package repo
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/Vatsal-Panjiar/delivery_management_system/internal/models"
+)
+
+type UserRepo struct {
+	DB *sqlx.DB
+}
+
+func NewUserRepo(db *sqlx.DB) *UserRepo {
+	return &UserRepo{DB: db}
+}
+
+// Create inserts a new user
+func (r *UserRepo) Create(u *models.User) error {
+	_, err := r.DB.Exec(
+		`INSERT INTO users (id, username, password_hash, role, created_at)
+		 VALUES ($1,$2,$3,$4,$5)`,
+		u.ID, u.Username, u.PasswordHash, u.Role, u.CreatedAt,
+	)
+	return err
+}
+
+// GetByUsername fetches a user by username
+func (r *UserRepo) GetByUsername(username string) (*models.User, error) {
+	var u models.User
+	err := r.DB.Get(&u, "SELECT * FROM users WHERE username=$1", username)
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
