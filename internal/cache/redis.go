315e2da110188dbdfe5f0f2b4925053a10e8a2cb
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 17:31:06 +0530

    Update redis.go

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,27 +1,35 @@
 package cache
 
-
 import (
-"context"
-"github.com/go-redis/redis/v8"
-"time"
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
-
-type RedisCache struct{
-Client *redis.Client
-Ctx context.Context
+type RedisCache struct {
+	client *redis.Client
+	ctx    context.Context
 }
 
-
-func NewRedis(addr string) *RedisCache {
-r := redis.NewClient(&redis.Options{Addr: addr})
-return &RedisCache{Client: r, Ctx: context.Background()}
+func NewRedisCache(addr string) *RedisCache {
+	r := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+	return &RedisCache{
+		client: r,
+		ctx:    context.Background(),
+	}
 }
 
-
-func (r *RedisCache) Set(key string, value interface{}, ttl time.Duration) error {
-return r.Client.Set(r.Ctx, key, value, ttl).Err()
+func (r *RedisCache) Set(key string, value []byte, ttl time.Duration) error {
+	return r.client.Set(r.ctx, key, value, ttl).Err()
 }
-func (r *RedisCache) Get(key string) (string, error) { return r.Client.Get(r.Ctx, key).Result() }
-func (r *RedisCache) Del(key string) error { return r.Client.Del(r.Ctx, key).Err() }
+
+func (r *RedisCache) Get(key string) (string, error) {
+	return r.client.Get(r.ctx, key).Result()
+}
+
+func (r *RedisCache) Del(key string) error {
+	return r.client.Del(r.ctx, key).Err()
+}
