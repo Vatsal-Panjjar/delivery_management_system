a6cd5f84538f3b058c6f3dfb93d61ba916bcdecb
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 22:55:07 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,50 +9,28 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 
+	"github.com/Vatsal-Panjiar/delivery_management_system/internal/auth"
 	"github.com/Vatsal-Panjiar/delivery_management_system/internal/cache"
 	"github.com/Vatsal-Panjiar/delivery_management_system/internal/handlers"
 	"github.com/Vatsal-Panjiar/delivery_management_system/internal/repo"
 )
 
 func main() {
-	// Connect to PostgreSQL
-	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=rupupuru@01 dbname=delivery sslmode=disable")
+	db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=YOUR_PASSWORD dbname=delivery sslmode=disable")
 	if err != nil {
-		log.Fatalf("DB connection failed: %v", err)
+		log.Fatal(err)
 	}
 
-	// Initialize repositories
-	userRepo := repo.NewUserRepo(db)
-	deliveryRepo := repo.NewDeliveryRepo(db)
-
-	// Initialize Redis cache
+	rRepo := repo.NewDeliveryRepo(db)
 	rCache := cache.NewRedisCache("localhost:6379")
 
-	// Initialize handlers
-	jwtSecret := []byte("your_super_secret_key")
-	authHandler := handlers.NewAuthHandler(userRepo, jwtSecret)
-	deliveryHandler := handlers.NewDeliveryHandler(deliveryRepo, rCache)
+	authHandler := auth.NewAuthHandler(rRepo, []byte("supersecretkey"))
+	deliveryHandler := handlers.NewDeliveryHandler(rRepo, rCache)
 
-	// Create router
-	router := chi.NewRouter()
+	r := chi.NewRouter()
+	auth.RegisterAuthRoutes(r, authHandler)
+	handlers.RegisterDeliveryRoutes(r, deliveryHandler)
 
-	// Auth routes
-	router.Post("/signup", authHandler.Signup)
-	router.Post("/login", authHandler.Login)
-
-	// Delivery routes
-	router.Post("/deliveries", deliveryHandler.Create)
-	router.Get("/deliveries/{id}", deliveryHandler.Get)
-	router.Get("/deliveries", deliveryHandler.ListByStatus)
-
-	// Serve static frontend files
-	router.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("frontend/static"))))
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "frontend/index.html")
-	})
-
-	fmt.Println("Server running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatalf("Server failed: %v", err)
-	}
+	fmt.Println("Server running on :8080")
+	http.ListenAndServe(":8080", r)
 }
