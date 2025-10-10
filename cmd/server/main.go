c269b8d40e5a534c717f7e57257f94d51ea0f472
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-10 15:15:54 +0530

    Create main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+module delivery_management_system
+
+
+go 1.21
+
+
+require (
+github.com/go-chi/chi/v5 v5.0.8
+github.com/go-redis/redis/v9 v9.0.0
+github.com/jmoiron/sqlx v1.4.10
+github.com/lib/pq v1.10.7
+github.com/golang-jwt/jwt/v5 v5.0.0
+github.com/google/uuid v1.4.0
+)
