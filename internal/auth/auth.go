4bd7c905aa90522787218c8b9e437556b0b24dd0
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-11 19:21:14 +0530

    Update auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,11 +1,22 @@
 package auth
 
 import (
+    "golang.org/x/crypto/bcrypt"
     "github.com/golang-jwt/jwt/v5"
     "time"
 )
 
-var jwtKey = []byte("supersecretkey") // put in env var ideally
+var jwtKey = []byte("supersecretkey")
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPassword(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
 
 func GenerateToken(userID, role string) (string, error) {
     claims := &jwt.MapClaims{
