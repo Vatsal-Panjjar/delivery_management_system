aedec1cd8ff9552d3025311f8fd1d834968c0e2a
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-11 18:29:39 +0530

    Update admin_handlers.go

--- internal/handlers/admin_handlers.go
+++ internal/handlers/admin_handlers.go
@@ -1,45 +1,23 @@
 package handlers
-import (
-    "encoding/json"
-    "net/http"
 
-    "github.com/jmoiron/sqlx"
-    "github.com/go-chi/chi/v5"
-    "github.com/Vatsal-Panjjar/delivery_management_system/internal/auth"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jmoiron/sqlx"
+	"github.com/go-redis/redis/v8"
 )
 
-func RegisterRoutes(r *chi.Mux, db *sqlx.DB) {
-    r.Post("/auth/register", func(w http.ResponseWriter, r *http.Request) {
-        var req struct {
-            Username string `json:"username"`
-            Password string `json:"password"`
-        }
-        json.NewDecoder(r.Body).Decode(&req)
-        hash, _ := auth.HashPassword(req.Password)
-        _, err := db.Exec("INSERT INTO users(username, password) VALUES($1,$2)", req.Username, hash)
-        if err != nil {
-            http.Error(w, "Registration failed", http.StatusBadRequest)
-            return
-        }
-        w.WriteHeader(http.StatusOK)
-    })
+type AdminHandler struct {
+	DB  *sqlx.DB
+	RDB *redis.Client
+}
 
-    r.Post("/auth/login", func(w http.ResponseWriter, r *http.Request) {
-        var req struct {
-            Username string `json:"username"`
-            Password string `json:"password"`
-        }
-        json.NewDecoder(r.Body).Decode(&req)
+func RegisterAdminRoutes(r chi.Router, db *sqlx.DB, rdb *redis.Client) {
+	h := &AdminHandler{DB: db, RDB: rdb}
+	r.Get("/admin/overview", h.AdminOverview)
+}
 
-        var hash string
-        err := db.QueryRow("SELECT password FROM users WHERE username=$1", req.Username).Scan(&hash)
-        if err != nil || !auth.CheckPassword(hash, req.Password) {
-            http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-            return
-        }
-
-        token, _ := auth.GenerateToken(req.Username)
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(map[string]string{"token": token})
-    })
+func (h *AdminHandler) AdminOverview(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Admin Dashboard"))
 }
