2181c18ef2a0b13486377ded18a7198c2321e765
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-11 19:16:06 +0530

    Create redis.go

---
+++ internal/cache/redis.go
@@ -0,0 +1,9 @@
+package cache
+
+import "github.com/go-redis/redis/v8"
+
+func NewRedisClient(addr string) *redis.Client {
+    return redis.NewClient(&redis.Options{
+        Addr: addr,
+    })
+}
