d23825ed1849257190231b52cce73a02ceb09013
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-11 19:24:06 +0530

    Update db.go

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,11 +1,19 @@
 package db
 
-import "github.com/jmoiron/sqlx"
+import (
+    "github.com/jmoiron/sqlx"
+    _ "github.com/lib/pq"
+    "log"
+    "os"
+)
 
-type Store struct {
-	DB *sqlx.DB
-}
+var DB *sqlx.DB
 
-func New(db *sqlx.DB) *Store {
-	return &Store{DB: db}
+func Connect() {
+    dbURL := os.Getenv("POSTGRES_URL") // e.g. postgres://user:pass@localhost:5432/delivery_db?sslmode=disable
+    var err error
+    DB, err = sqlx.Connect("postgres", dbURL)
+    if err != nil {
+        log.Fatalf("Failed to connect to Postgres: %v", err)
+    }
 }
