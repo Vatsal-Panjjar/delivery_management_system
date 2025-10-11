021d7eabfa71e0561088d2a6ef3bd67ba3bd9156
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-11 15:55:47 +0530

    Update auth_middleware.go

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,6 +1,7 @@
-package middleware
+package handlers
 
 import (
+    "context"
     "net/http"
 
     "github.com/Vatsal-Panjjar/delivery_management_system/internal/auth"
@@ -8,11 +9,19 @@ import (
 
 func AuthMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        _, err := auth.VerifyToken(r)
-        if err != nil {
-            http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
+        token := r.Header.Get("Authorization")
+        if token == "" {
+            http.Error(w, "Missing token", http.StatusUnauthorized)
             return
         }
-        next.ServeHTTP(w, r)
+
+        claims, err := auth.ParseToken(token[len("Bearer "):])
+        if err != nil {
+            http.Error(w, "Invalid token", http.StatusUnauthorized)
+            return
+        }
+
+        ctx := context.WithValue(r.Context(), "user", claims)
+        next.ServeHTTP(w, r.WithContext(ctx))
     })
 }
