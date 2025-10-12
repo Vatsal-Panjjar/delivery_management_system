aae5bce778f1f4bfe5534454e63ab9d7047e76e7
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-10-12 12:08:22 +0530

    Update auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,42 +1,30 @@
 package auth
 
 import (
-    "golang.org/x/crypto/bcrypt"
-    "github.com/golang-jwt/jwt/v5"
-    "time"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtKey = []byte("supersecretkey")
+var jwtSecret = []byte("secret-key")
 
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
 }
 
-func CheckPassword(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+func CheckPassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
-func GenerateToken(userID, role string) (string, error) {
-    claims := &jwt.MapClaims{
-        "user_id": userID,
-        "role":    role,
-        "exp":     time.Now().Add(time.Hour * 24).Unix(),
-    }
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtKey)
-}
-
-func ParseToken(tokenString string) (*jwt.MapClaims, error) {
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
-    if err != nil {
-        return nil, err
-    }
-    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-        return &claims, nil
-    }
-    return nil, nil
+func GenerateToken(username, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"username": username,
+		"role":     role,
+		"exp":      time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
 }
